3c7c697b02b58dd1682d84daa3d63b78df6499ed
tlancaster6 <tuckerlancaster@gmail.com>
2022-10-26 14:17:42 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from yolov5.detect import run as run_detection
+import cv2
+
+pipe_weights = 'justpipe.pt'
+fish_weights = 'justfish.pt'
+
+
+def detect(source, weights, device=0, save_txt=True, **kwargs):
+    run_detection(source=source, weights=weights, device=device, save_txt=save_txt, **kwargs)
+
+
+
