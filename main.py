40cd1fc23e182c76d89e04c3b74403dd2a4b3df4
tlancaster <tlancaster6@gatech.edu>
2022-11-16 13:07:06 -0500

    new plotting funcitons

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# from yolov5.detect import run as run_detection
-from plotting import plotters
-
-pipe_weights = 'justpipe.pt'
-fish_weights = 'justfish.pt'
-
-
-def detect(source, weights, device=0, save_txt=True, **kwargs):
-    run_detection(source=source, weights=weights, device=device, save_txt=save_txt, **kwargs)
-
-
-
+from plotting.plotters import *
+plot_hit_line('knl_t013_demasoni_110722')
+# plot_hit_scatter('knl_t013_demasoni_110722')
--- plotting/plotters.py
+++ plotting/plotters.py
@@ -1,8 +1,41 @@
+import numpy as np
 import seaborn as sns
 import pandas as pd
 from definitions import PROJ_DIR
+import matplotlib.pyplot as plt
+from glob import glob
+import datetime as dt
 
-def plot_hit_records(project):
-    hit_records = (PROJ_DIR / project / 'HitRecords').iterdir()
-    df = pd.concat([pd.read_csv(hr) for hr in hit_records])
-    return df
+TESTING = True
+
+def plot_hit_line(project):
+    hit_records = sorted(glob(str(PROJ_DIR / project / 'HitRecords' / '*.csv')))
+    n_records = len(hit_records)
+    fig, axes = plt.subplots(n_records, 1, figsize=(8.5, 2*n_records))
+    axes = axes.flatten()
+    for i, hr in enumerate(hit_records):
+        df = pd.read_csv(hr)
+        df['datetime'] = pd.to_datetime(df['time_ms'], unit='ms') - dt.timedelta(hours=5)
+        sns.lineplot(df, x='datetime', y='count', ax=axes[i], linewidth=0.5)
+        axes[i].axhline(y=20.0, color='r', linestyle='--')
+    fig.suptitle(f'Spawning Activity for {project}')
+    fig.tight_layout()
+    fig.savefig(PROJ_DIR / project / 'HitRecords' / 'hit_plot.pdf')
+
+
+def plot_hit_scatter(project):
+    hit_records = sorted(glob(str(PROJ_DIR / project / 'HitRecords' / '*.csv')))
+    n_records = len(hit_records)
+    fig, axes = plt.subplots(n_records, 1, figsize=(8.5, 2*n_records))
+    axes = axes.flatten()
+    for i, hr in enumerate(hit_records):
+        df = pd.read_csv(hr)
+        df['datetime'] = pd.to_datetime(df['time_ms'], unit='ms') - dt.timedelta(hours=5)
+        df['above_thresh'] = df['count'] >= 20
+        sns.scatterplot(df, x='datetime', y='count', ax=axes[i], linewidth=0, size=2, hue='above_thresh', alpha=0.25, legend=False)
+        axes[i].axhline(y=20.0, color='r', linestyle='--')
+    fig.suptitle(f'Spawning Activity for {project}')
+    fig.tight_layout()
+    fig.savefig(PROJ_DIR / project / 'HitRecords' / 'hit_scatter.pdf')
+
+
